c02063229b881f491cc1f30a81d8aaaa86e648c9
Gabriel Aguero <gabrielhaguero@gmail.com>
2023-07-27 00:20:31 -0300

    agregando lista con FlatList y modal

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { Button, StyleSheet, Text, TextInput, View, StatusBar, SafeAreaView } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, StatusBar, SafeAreaView, FlatList, TouchableOpacity, Modal } from 'react-native';
 import { styles } from './styles';
 import { useState } from 'react';
 
 export default function App() {
 
-  const [tast, setTask] = useState('')
+  const [task, setTask] = useState('')
   const [listTasks, setListTasks] = useState([])
   const [borderColor, setBorderColor] = useState('#C5C9E7')
+  const [isVisible, setIsVisible] = useState(false)
+  const [selectedTask, setSelectedTask] = useState(null)
 
   const onHandlerFocus = () => {
     setBorderColor('#424D9E')
@@ -28,8 +30,28 @@ export default function App() {
       }
     ])
 
-    setTask('')
+    setTask(' ')
   }
+
+  const onHandlerModal = (item) => {
+    setIsVisible(true)
+    setSelectedTask(item)
+  }
+
+  const onHandleDelete = (id) => {
+    setListTasks((prevList) => prevList.filter((task) => task.id !== id ))
+    setIsVisible(false)
+  }
+
+  const renderItem = ({item}) =>(
+    <TouchableOpacity onPress={() => onHandlerModal(item)} style={styles.containerItem}>
+      <View style={styles.taskItemCheck}>
+        <Text style={styles.iconAdd}>👍</Text>
+        <Text style={styles.listItem}> {item.value} </Text>
+      </View>
+      <Text style={styles.icon}>X</Text>
+    </TouchableOpacity>
+  )
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -46,19 +68,42 @@ export default function App() {
             onFocus={onHandlerFocus}
             onBlur={onHandlerBlur}
             onChangeText={onHandlerChangeText}
+            value={task}
           />
-          <Button title="Create" color='#424D9E' onPress={onHandlerCreateTask}/>
-          <View style={styles.listContainer}>
-            {
-              listTasks.map((item) =>{
-              <View style={styles.containerItem} key={item-id}>
-                <Text style={styles.listItem}> {item.value} </Text>
-              </View>
-            })}
-          </View>
        
+          <Button disabled={task.length === 0} title="Create" color='#424D9E' onPress={onHandlerCreateTask}/>
         </View>
+        <View style={styles.listContainer}>
+          <FlatList 
+            data={listTasks}
+            renderItem={renderItem}
+            contentContainerStyle={styles.list}
+            alwaysBounceVertical={false}
+            keyExtractor={item => item.id}
+          />
+        </View>  
       </View>
+      <Modal visible={isVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task Detail</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>Estas Seguro que deseas eliminar esta tarea ?</Text>
+            <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button 
+              title='Cancel'
+              color='#424D9E'
+              onPress={() => setIsVisible(false)}
+            />
+            <Button 
+              title='Delete'
+              color='red'
+              onPress={() => onHandleDelete(selectedTask?.id)}
+            />
+          </View>
+         </View>
+      </Modal>
     </SafeAreaView>
   );
 }
